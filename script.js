fe67f7f0a3c01c4af9733060d4bb5cdccaff35c5
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-06-10 13:08:27 -0300

    [ADD] agrego lo hecho ayer.

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    document.getElementById("boton").addEventListener("click", function() {
+        document.querySelector("body").classList.toggle("dark");
+    });
+})
